49a47f2a9f57b1ba5f36904e2d77ba3a6f5a2130
speedyibbi <ibbi2k2.khan@gmail.com>
2025-09-14 01:10:08 +0500

    build: initialize project

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <main>
+    <img src="/logo.png" alt="logo" />
+    <h1>Runestone</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000015;
+}
+
+h1 {
+  color: #ccaaff;
+  font-size: 4rem;
+  font-family: "Passero One", serif;
+  font-weight: 900;
+  filter: drop-shadow(0 0 0.1rem #ccaaff);
+}
+
+img {
+  width: 200px;
+  filter: drop-shadow(0 0 0.5rem #ffffff);
+  transition: filter 0.5s ease-out;
+  animation: float 4s ease-in-out infinite
+}
+
+img:hover {
+  filter: drop-shadow(0 0 15rem #ffffff00);
+}
+
+@keyframes float {
+  0%   { transform: translateY(0); }
+  50%  { transform: translateY(-16px); }
+  100% { transform: translateY(0); }
+}
+</style>
+
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+    server: {
+      port: env.PORT ? Number(env.PORT) : 5173,
+    },
+  }
+})
+
