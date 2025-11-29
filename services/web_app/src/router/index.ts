c3c7b214f4cd057fae1480ea077afb8a9f61a17b
speedyibbi <ibbi2k2.khan@gmail.com>
2025-11-29 18:45:34 +0500

    feat: add codex view and update routing

--- services/web_app/src/router/index.ts
+++ services/web_app/src/router/index.ts
@@ -1,18 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Auth from '@/views/Auth.vue'
+import Codex from '@/views/Codex.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/auth',
       name: 'auth',
       meta: {
         title: 'Unlock Vault',
         description: 'Unlock your vault to access your data',
       },
       component: Auth,
+    },
+    {
+      path: '/',
+      name: 'codex',
+      meta: {
+        title: 'Codex',
+        description: 'Your personal knowledge base',
+      },
+      component: Codex,
     },
   ],
 })
---
+++ services/web_app/src/views/Codex.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div>
+      <h1>Codex</h1>
+    </div>
+</template>
+
+<style scoped>
+</style>
