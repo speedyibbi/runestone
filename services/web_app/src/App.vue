8c2787bbe9d8150ad6e32c62e814dc94d532a196
speedyibbi <ibbi2k2.khan@gmail.com>
2025-12-19 00:57:18 +0500

    feat: add page transition effects

--- services/web_app/src/App.vue
+++ services/web_app/src/App.vue
@@ -4,6 +4,23 @@ import Wrapper from '@/components/layout/Wrapper.vue'
 
 <template>
   <Wrapper>
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page-fade" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </Wrapper>
 </template>
+
+<style>
+/* Page transition styles */
+.page-fade-enter-active,
+.page-fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.page-fade-enter-from,
+.page-fade-leave-to {
+  opacity: 0;
+}
+</style>
