f131885cd4ae3dae9b641c14c9904f0d320accec
speedyibbi <ibbi2k2.khan@gmail.com>
2026-01-13 01:17:58 +0500

    feat: add sync feature flag to configuration

--- services/config/src/index.ts
+++ services/config/src/index.ts
@@ -38,6 +38,7 @@ export const config = Object.freeze({
       cryptography: process.env.FEATURE_CRYPTOGRAPHY ? process.env.FEATURE_CRYPTOGRAPHY === 'true' : true,
       ftsSearch: process.env.FEATURE_FTS_SEARCH ? process.env.FEATURE_FTS_SEARCH === 'true' : true,
       graph: process.env.FEATURE_GRAPH ? process.env.FEATURE_GRAPH === 'true' : false,
+      sync: process.env.FEATURE_SYNC ? process.env.FEATURE_SYNC === 'true' : false,
     },
   },
   aws: {
